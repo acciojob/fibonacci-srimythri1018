3039253ca2c264b2a3a9953484eca71ec4336c48
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-07 18:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function fibonacci(num) {
- if (num === 0) {
+ if (num === 1) {
     return 0;
-  } else if (num === 1) {
-    return 1;
-  } else {
+  } 
+   else {
     let fib = [0, 1];
     for (let i = 2; i <= num; i++) {
       fib[i] = fib[i-1] + fib[i-2];
